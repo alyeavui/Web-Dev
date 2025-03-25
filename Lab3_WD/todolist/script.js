8abf154d5edee82c27d6eed063ce7a6ddd91e325
Ayaulym Zhuniskhan <ayaulym.zhuniskhan89@gmail.com>
2025-03-25 21:08:00 +0500

    lab8

--- Lab3_WD/todolist/script.js
+++ Lab3_WD/todolist/script.js
@@ -5,6 +5,7 @@ const taskList = document.querySelector("#tasks");
 let tasks = [];
 
 function addTask(){
+    if (taskInput.value.trim() === "") return;
     tasks.push({ name: taskInput.value, done: false });
     renderTasks();
     taskInput.value = "";
@@ -16,33 +17,35 @@ function renderTasks(){
     const taskItem = document.createElement("li");
     taskItem.classList.add("task-item");
     taskItem.innerHTML =
-      `<div class="outtask">
-      <input type="checkbox"  class="task-done" />
-      <div  class="item ${task.done ? "done" : ""}">${task.name}</div>
-      <div onclick="deleteTask(${index})"><i class="fa-solid fa-trash" style="color:red;"></i></div>
+      `<div class="task-container">
+        <input type="checkbox" class="task-done" ${task.done ? "checked" : ""} />
+        <span class="item ${task.done ? "done" : ""}">${task.name}</span>
+        <button class="delete-btn" onclick="deleteTask(${index})"><i class="fa-solid fa-trash" style="color:red;"></i></button>
       </div>`;
+    
     taskItem.querySelector(".task-done").addEventListener("click", () => {
       markTaskDone(index);
     });
     taskList.appendChild(taskItem);
   });
-};
+}
 
 function deleteTask(index){
   tasks.splice(index, 1);
   renderTasks();
-};
+}
+
 function markTaskDone(index){
-    if(tasks[index].done){
-        tasks[index].done=false
-    }
-    else{
-        tasks[index].done = true;
-    }
-  
-  renderTasks();
-};
+    tasks[index].done = !tasks[index].done;
+    renderTasks();
+}
 
 addTaskBtn.addEventListener("click", addTask);
+
+taskInput.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        addTask();
+    }
+});
 
 renderTasks();
